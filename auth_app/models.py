69feae3bd2b0a25f18789dbb5718fd0e46fc2636
Gosia2024 <lm.sprachen@gmail.com>
2026-01-15 18:35:15 +0100

    feat:add custom user model

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, fullname, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        if not fullname:
+            raise ValueError("Fullname is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, fullname=fullname, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, fullname, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        return self.create_user(email=email, fullname=fullname, password=password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    fullname = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["fullname"]
+
+    def __str__(self):
+        return self.email
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+      # Third-party
+    "rest_framework",
+    "rest_framework.authtoken",
+
+    # Local apps
+    "auth_app",
+    "boards_app",
+    "tasks_app",
+    "comments_app",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +129,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "auth_app.User"
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
