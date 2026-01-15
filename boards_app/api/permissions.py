b7e8d30d65670fd089b3fcf36ee1e4c28cd45a58
Gosia2024 <lm.sprachen@gmail.com>
2026-01-15 23:08:25 +0100

    feat: add boards endpoints (with comments)

---
+++ boards_app/api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsBoardMemberOrOwner(BasePermission):
+    """Allows access if the user is board owner or listed as a member."""
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner or obj.members.filter(id=request.user.id).exists()
+
+
+class IsBoardOwner(BasePermission):
+    """Allows access only to the board owner."""
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner
